e9ad6af87ee735648716dc3b48f51ed39b30f4f4
Toni <toni.martikainen90@gmail.com>
2016-01-15 14:10:19 +0200

    Update ReceiveThread.java

--- NetworkChat/RacingGame-client/src/racinggame/ReceiveThread.java
+++ NetworkChat/RacingGame-client/src/racinggame/ReceiveThread.java
@@ -8,14 +8,48 @@ import java.util.Scanner;
 import java.io.*;
 import java.net.*;
 
-public class ReceiveThread implements Runnable {
-    private Socket sock=null;
-    private BufferedReader receive=null;
-    public ReceiveThread(Socket sock){
-        this.sock=sock;
+
+
+class ReceiveThread extends Thread {
+ 
+    private DatagramSocket udpClientSocket;
+    private boolean stopped = false;
+ 
+    public ReceiveThread(DatagramSocket ds) throws SocketException {
+        this.udpClientSocket = ds;
     }
-    public void run(){
-        
+ 
+    public void halt() {
+        this.stopped = true;
     }
-    
+ 
+    public void run() {
+ 
+        // Create a byte buffer/array for the receive Datagram packet
+        byte[] receiveData = new byte[1024];
+ 
+        while (true) {            
+            if (stopped)
+            return;
+ 
+            // Set up a DatagramPacket to receive the data into
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            //System.out.println("I am in the reader!");
+            try {
+                // Receive a packet from the server (blocks until the packets are received)
+                udpClientSocket.receive(receivePacket);
+                //System.out.println("Am i receiving?");
+                // Extract the reply from the DatagramPacket      
+                String serverReply =  new String(receivePacket.getData(), 0, receivePacket.getLength());
+ 
+                // print to the screen
+                System.out.println("Recieved msg: \"" + serverReply + "\"\n");
+ 
+                Thread.yield();
+            } 
+            catch (IOException ex) {
+            System.err.println(ex);
+            }
+        }
+    }
 }
