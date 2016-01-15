3c463f737496753b70e0d501e2d0756d52abe2e6
Toni <toni.martikainen90@gmail.com>
2016-01-15 14:10:48 +0200

    Update SendThread.java

--- NetworkChat/RacingGame-client/src/racinggame/SendThread.java
+++ NetworkChat/RacingGame-client/src/racinggame/SendThread.java
@@ -4,38 +4,70 @@
  * and open the template in the editor.
  */
 package racinggame;
-import java.util.Scanner;
 import java.io.*;
 import java.net.*;
 
 
-public class SendThread implements Runnable {
-    Socket sock=null;
-    PrintWriter print=null;
-    BufferedReader brinput=null;
-
-    public SendThread(Socket sock)
-    {
-	this.sock = sock;
-    }//end constructor
-    
-    public void run(){
-	try{
-            if(sock.isConnected())
-		{
-                    System.out.println("Client connected to "+sock.getInetAddress() + " on port "+sock.getPort());
-                    this.print = new PrintWriter(sock.getOutputStream(), true);	
-		while(true){
-                    System.out.println("Type your message to send to server(type EXIT to exit): ");
-                    brinput = new BufferedReader(new InputStreamReader(System.in));
-                    String msgtoServerString=null;
-                    msgtoServerString = brinput.readLine();
-                    this.print.println(msgtoServerString);
-                    this.print.flush();
-		
-                    if(msgtoServerString.equals("EXIT"))
-                        break;
-		}//end while
-		sock.close();}}catch(Exception e){System.out.println(e.getMessage());}
-	}//end run method
-}//end class
+class SendThread extends Thread {
+ 
+    private InetAddress serverIPAddress;
+    private DatagramSocket udpClientSocket;
+    private boolean stopped = false;
+    private int serverport;
+ 
+    public SendThread(InetAddress address, int serverport) throws SocketException {
+        this.serverIPAddress = address;
+        this.serverport = serverport;
+        // Create client DatagramSocket
+        this.udpClientSocket = new DatagramSocket();
+        this.udpClientSocket.connect(serverIPAddress, serverport);
+    }
+    public void halt() {
+        this.stopped = true;
+    }
+    public DatagramSocket getSocket() {
+        return this.udpClientSocket;
+    }
+ 
+    public void run() {       
+        try {    
+        	//send blank message
+        	byte[] data = new byte[1024];
+        	data = "".getBytes();
+        	DatagramPacket blankPacket = new DatagramPacket(data,data.length , serverIPAddress, serverport);
+            udpClientSocket.send(blankPacket);
+            
+        	// Create input stream
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            while (true) 
+            {
+                if (stopped)
+                    return;
+ 
+                // Message to send
+                String clientMessage = inFromUser.readLine();
+ 
+                if (clientMessage.equals("."))
+                    break;
+ 
+                // Create byte buffer to hold the message to send
+                byte[] sendData = new byte[1024];
+ 
+                // Put this message into our empty buffer/array of bytes
+                sendData = clientMessage.getBytes();
+ 
+                // Create a DatagramPacket with the data, IP address and port number
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverIPAddress, serverport);
+ 
+                // Send the UDP packet to server
+                //System.out.println("I just sent: "+clientMessage);
+                udpClientSocket.send(sendPacket);
+ 
+                Thread.yield();
+            }
+        }
+        catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+} 
